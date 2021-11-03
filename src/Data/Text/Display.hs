703b9eacf64e18998d1d2752f771bd450a0abb93
Hécate Moonlight <hecate+github@glitchbra.in>
2021-11-03 12:19:05 +0100

    Fix Haddock quotes

--- src/Data/Text/Display.hs
+++ src/Data/Text/Display.hs
@@ -151,7 +151,7 @@ display a = TL.toStrict $ TB.toLazyText $ displayBuilder a
 -- | 🚫 You should not try to display functions!
 --
 -- 💡 Write a 'newtype' wrapper that represents your domain more accurately.
---    If you are not consciously trying to use `display` on a function,
+--    If you are not consciously trying to use 'display' on a function,
 --    make sure that you are not missing an argument somewhere.
 --
 -- @since 0.0.1.0
@@ -381,9 +381,9 @@ instance (Display a, Display b, Display c, Display d) => Display (a, b, c, d) wh
 --
 -- === A “Lawless Typeclass”
 --
--- The `Display` typeclass does not contain any law. This is a controversial choice for some people,
+-- The 'Display' typeclass does not contain any law. This is a controversial choice for some people,
 -- but the truth is that there are not any laws to ask of the consumer that are not already enforced
--- by the type system and the internals of the `Data.Text.Internal.Text` type.
+-- by the type system and the internals of the 'Data.Text.Internal.Text' type.
 --
 -- === "🚫 You should not try to display functions!"
 --
@@ -394,13 +394,13 @@ instance (Display a, Display b, Display c, Display d) => Display (a, b, c, d) wh
 -- >      If you are not consciously trying to use `display` on a function,
 -- >      make sure that you are not missing an argument somewhere.
 --
--- The `display` library does not allow the definition and usage of `Display` on
+-- The 'display' library does not allow the definition and usage of 'Display' on
 -- bare function types (@(a -> b)@).
 -- Experience and time have shown that due to partial application being baked in the language,
 -- many users encounter a partial application-related error message when a simple missing
 -- argument to a function is the root cause.
 --
--- There may be legitimate uses of a `Display` instance on a function type.
+-- There may be legitimate uses of a 'Display' instance on a function type.
 -- But these usages are extremely dependent on their domain of application.
 -- That is why it is best to wrap them in a newtype that can better
 -- express and enforce the domain.
@@ -410,5 +410,5 @@ instance (Display a, Display b, Display c, Display d) => Display (a, b, c, d) wh
 -- An arbitrary ByteStrings cannot be safely converted to text without prior knowledge of its encoding.
 --
 -- As such, in order to avoid dangerously blind conversions, it is recommended to use a specialised
--- function such as `Data.Text.Encoding.decodeUtf8'` or `Data.Text.Encoding.decodeUtf8With` if you wish to turn a UTF8-encoded ByteString
+-- function such as 'Data.Text.Encoding.decodeUtf8'' or 'Data.Text.Encoding.decodeUtf8With' if you wish to turn a UTF8-encoded ByteString
 -- to Text.
