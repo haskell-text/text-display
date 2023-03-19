a7c311b10f99be7e9187e69ef5017c22ae01dfd6
Hécate Moonlight <hecate+github@glitchbra.in>
2023-03-19 17:26:09 +0100

    WIP: Migration to Tasty

--- test/Main.hs
+++ test/Main.hs
@@ -18,14 +18,27 @@ import Data.Text.Arbitrary
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Builder as TB
 import System.Timeout
-import Test.Hspec
-import Test.Hspec.QuickCheck
 import Test.ShouldNotTypecheck (shouldNotTypecheck)
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
 
 import Data.Text.Display
 
 main :: IO ()
-main = hspec spec
+main = defaultMain spec
+
+shouldThrow :: (HasCallStack, Exception e, Eq e) => IO a -> e -> Assertion
+shouldThrow action expectedException = do
+  result <- try action
+  case result of
+    Right _ ->
+      assertFailure $ "Expected test to fail with " <> show expectedException
+    Left actualException ->
+      assertEqual
+        ("Expected test to fail with" <> show expectedException <> " but failed with " <> show actualException <> " instead.")
+        expectedException
+        actualException
 
 data AutomaticallyDerived = AD
   deriving stock (Show)
@@ -43,72 +56,80 @@ data OpaqueType = OpaqueType Int
 
 -- | @v \`shouldEvaluateWithin\` n@ sets the expectation that evaluating @v@
 -- should take no longer than @n@ microseconds.
-shouldEvaluateWithin :: (HasCallStack, NFData a) => a -> Int -> Expectation
+shouldEvaluateWithin :: (HasCallStack, NFData a) => a -> Int -> Assertion
 shouldEvaluateWithin a n = do
   res <- timeout n (evaluate $ force a)
   when (isNothing res) $ do
-    expectationFailure ("evaluation timed out in " <> show n <> " microseconds")
+    assertFailure ("evaluation timed out in " <> show n <> " microseconds")
 
-spec :: Spec
-spec = do
-  describe "Display Tests:" $ do
-    it "Display instance for Text stays the same" $
-      display ("3" :: Text) `shouldBe` ("3" :: Text)
-    it "Deriving via its own Show instance works" $
-      T.unpack (display AD) `shouldBe` show AD
-    it "Opaque types stay opaque" $
-      display (OpaqueType 3 :: OpaqueType) `shouldBe` "<opaque>"
-    it "Manual instance is stays the same" $
-      display (MT 32) `shouldBe` "MT 32"
-    it "List instance is equivalent to Show" $ do
-      let list = [1 .. 5] :: [Int]
-      T.unpack (display list) `shouldBe` show list
-    it "Single-element List instance is equivalent to Show" $ do
-      let list = [1] :: [Int]
-      T.unpack (display list) `shouldBe` show list
-    it "List instance is streamed lazily" $ do
-      let list = [1 ..] :: [Int]
-      TL.take 20 (TB.toLazyText $ displayBuilder list) `shouldEvaluateWithin` 100000
-    it "NonEmpty instance is equivalent to Show" $ do
-      let ne = NE.fromList [1 .. 5] :: NonEmpty Int
-      T.unpack (display ne) `shouldBe` show ne
-    it "Just True instance is equivalent to Show" $ do
-      T.unpack (display (Just True)) `shouldBe` show (Just True)
-    it "Nested Maybe instance is equivalent to Show" $ do
-      let nestedMaybe = Just (Just 5) :: Maybe (Maybe Int)
-      T.unpack (display nestedMaybe) `shouldBe` show nestedMaybe
-    it "Nothing instance is equivalent to Show" $ do
-      T.unpack (display (Nothing @Bool)) `shouldBe` show (Nothing @Bool)
-    it "Char '\'' instance is equivalent to Text" $ do
-      display '\'' `shouldBe` T.singleton '\''
-    it "2-Tuple instance is equivalent to Show" $ do
-      let tuple = (1 :: Int, True)
-      T.unpack (display tuple) `shouldBe` show tuple
-    it "3-Tuple instance is equivalent to Show" $ do
-      let tuple = (1 :: Int, True, "hahahha" :: String)
-      display tuple `shouldBe` "(" <> display (1 :: Int) <> "," <> display True <> "," <> display @String "hahahha" <> ")"
-
-  describe "Display props:" $ do
-    prop "Text instance stays the same" $ do
-      \string -> display (string :: Text) `shouldBe` string
-    prop "String instance is equivalent to Text" $ do
-      \string -> display (string :: String) `shouldBe` T.pack string
-    prop "Chars are packed" $
-      \c -> display (c :: Char) `shouldBe` T.singleton c
-
-  describe "Forbidden instances" $ do
-    it "Should not compile for a function instance" $
-      shouldNotTypecheck (display id) `shouldThrow` anyErrorCall
-    it "Should not compile for ByteStrings" $
-      let bs = "badstring" :: ByteString
-       in shouldNotTypecheck (display bs) `shouldThrow` anyErrorCall
-
-  describe "displayParen tests" $ do
-    it "Surrounds with parens when True" $
-      displayParen True "foo" `shouldBe` "(foo)"
-    it "Doesn't surround with parens when False" $
-      displayParen False "foo" `shouldBe` "foo"
-    it "Surrounds deeply-nested Maybes with a prec of 10" $
-      displayPrec 10 (Just (Just (Just (3 :: Int)))) `shouldBe` "Just (Just (Just 3))"
-    it "Surrounds deeply-nested Maybes with a prec of 11" $
-      displayPrec 11 (Just (Just (Just (3 :: Int)))) `shouldBe` "(Just (Just (Just 3)))"
+spec :: TestTree
+spec =
+  testGroup
+    "Display Tests"
+    [ testGroup
+        "Instances Tests"
+        [ testCase "Display instance for Text stays the same" $
+            display ("3" :: Text) @?= ("3" :: Text)
+        , testCase "Deriving via its own Show instance works" $
+            T.unpack (display AD) @?= show AD
+        , testCase "Opaque types stay opaque" $
+            display (OpaqueType 3 :: OpaqueType) @?= "<opaque>"
+        , testCase "Manual instance is stays the same" $
+            display (MT 32) @?= "MT 32"
+        , testCase "List instance is equivalent to Show" $ do
+            let list = [1 .. 5] :: [Int]
+            T.unpack (display list) @?= show list
+        , testCase "Single-element List instance is equivalent to Show" $ do
+            let list = [1] :: [Int]
+            T.unpack (display list) @?= show list
+        , testCase "List instance is streamed lazily" $ do
+            let list = [1 ..] :: [Int]
+            TL.take 20 (TB.toLazyText $ displayBuilder list) `shouldEvaluateWithin` 100000
+        , testCase "NonEmpty instance is equivalent to Show" $ do
+            let ne = NE.fromList [1 .. 5] :: NonEmpty Int
+            T.unpack (display ne) @?= show ne
+        , testCase "Just True instance is equivalent to Show" $ do
+            T.unpack (display (Just True)) @?= show (Just True)
+        , testCase "Nested Maybe instance is equivalent to Show" $ do
+            let nestedMaybe = Just (Just 5) :: Maybe (Maybe Int)
+            T.unpack (display nestedMaybe) @?= show nestedMaybe
+        , testCase "Nothing instance is equivalent to Show" $ do
+            T.unpack (display (Nothing @Bool)) @?= show (Nothing @Bool)
+        , testCase "Char '\'' instance is equivalent to Text" $ do
+            display '\'' @?= T.singleton '\''
+        , testCase "2-Tuple instance is equivalent to Show" $ do
+            let tuple = (1 :: Int, True)
+            T.unpack (display tuple) @?= show tuple
+        , testCase "3-Tuple instance is equivalent to Show" $ do
+            let tuple = (1 :: Int, True, "hahahha" :: String)
+            display tuple @?= "(" <> display (1 :: Int) <> "," <> display True <> "," <> display @String "hahahha" <> ")"
+        ]
+    , testGroup
+        "Properties Tests"
+        [ testProperty "Text instance stays the same" $ do
+            \string -> display (string :: Text) === string
+        , testProperty "String instance is equivalent to Text" $ do
+            \string -> display (string :: String) === T.pack string
+        , testProperty "Chars are packed" $
+            \c -> display (c :: Char) === T.singleton c
+        ]
+    , testGroup
+        "Forbidden Instances"
+        [ testCase "Should not compile for a function instance" $
+            shouldNotTypecheck (display id) -- `shouldThrow` SomeException
+        , testCase "Should not compile for ByteStrings" $
+            let bs = "badstring" :: ByteString
+             in shouldNotTypecheck (display bs) -- `shouldThrow` SomeException
+        ]
+    , testGroup
+        "`displayParen` tests"
+        [ testCase "Surrounds with parens when True" $
+            displayParen True "foo" @?= "(foo)"
+        , testCase "Doesn't surround with parens when False" $
+            displayParen False "foo" @?= "foo"
+        , testCase "Surrounds deeply-nested Maybes with a prec of 10" $
+            displayPrec 10 (Just (Just (Just (3 :: Int)))) @?= "Just (Just (Just 3))"
+        , testCase "Surrounds deeply-nested Maybes with a prec of 11" $
+            displayPrec 11 (Just (Just (Just (3 :: Int)))) @?= "(Just (Just (Just 3)))"
+        ]
+    ]
